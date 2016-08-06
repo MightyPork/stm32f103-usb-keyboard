f1abfedd1c9cf920ba5957cea22ae02821c6c520
Ondřej Hruška <ondra@ondrovo.com>
2016-08-07 00:54:06 +0200

    format

--- Src/usbd_desc.c
+++ Src/usbd_desc.c
@@ -58,9 +58,9 @@
   */ 
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
+#define USBD_MANUFACTURER_STRING     "MightyPork"
 #define USBD_PID_FS     22315
-#define USBD_PRODUCT_STRING_FS     "STM32 Human interface"
+#define USBD_PRODUCT_STRING_FS     "Screen Rotation Sensor"
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 #define USBD_CONFIGURATION_STRING_FS     "HID Config"
 #define USBD_INTERFACE_STRING_FS     "HID Interface"
